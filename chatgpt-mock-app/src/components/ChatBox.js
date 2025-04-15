a1e12e1768c101bf7d40ff50faa922a2f7dce7f5
kfir.shabi <kfir571@gmail.com>
2025-04-15 15:13:11 +0300

    fix mobile view

--- chatgpt-mock-app/src/components/ChatBox.js
+++ chatgpt-mock-app/src/components/ChatBox.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import MessageList from "./MessageList";
 import ChatInput from "./ChatInput";
 import { sendToGPTApi } from "../services/gptService";
@@ -6,6 +6,7 @@ import { sendToGPTApi } from "../services/gptService";
 function ChatBox({collapsed}) {
     const [messages, setMessages] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const topRef = useRef(null);
 
     const sendToGPT = async (queryText) => {
         try {
@@ -34,10 +35,11 @@ function ChatBox({collapsed}) {
     const handelGetNewMessage = (newMessage) => {
         setMessages((prevMessages) => [...prevMessages, newMessage]);
         sendToGPT(newMessage.message);
+        topRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
     }
 
     return (
-        <div className={`main-page ${collapsed ? "collapsed" : ""}`}>
+        <div className={`main-page ${collapsed ? "collapsed" : ""}` } ref={topRef}>
             <MessageList messages={messages} />
             <ChatInput onAddMessage={handelGetNewMessage} isLoading={isLoading} />
         </div>
